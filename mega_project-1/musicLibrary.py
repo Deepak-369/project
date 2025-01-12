db5b411a0cc3115df07da50ced34f05744225cea
Deepak-369 <deepakkumarswain369@gmail.com>
2025-01-13 00:07:40 +0530

    Add files via upload

---
+++ mega_project-1/musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "nali": "https://www.youtube.com/watch?v=gy10xDCQP_E",
+    "senorita" : "https://www.youtube.com/watch?v=Pkh8UtuejGw",
+    "jagah" : "https://www.youtube.com/watch?v=M33j0t2W6zM",
+    "big dogs" :"https://www.youtube.com/watch?v=hOHKltAiKXQ",
+    "intention" : "https://www.youtube.com/watch?v=3AyMjyHu1bA"
+}
